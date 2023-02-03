8c23a4ec4b40f6f3e47d53cb0058ac9fa9c0d7c7
ilias <mail@ilias.xyz>
2023-02-03 15:26:42 +0100

    Install elm

--- shell.nix
+++ shell.nix
@@ -2,6 +2,6 @@ let
   sources = import ./nix/sources.nix { };
   pkgs = import sources.nixpkgs { };
 in pkgs.mkShell {
-  buildInputs = [pkgs.stack];
+  buildInputs = [pkgs.stack pkgs.elmPackages.elm];
   NIX_PATH = "nixpkgs=${pkgs.path}";
 }
