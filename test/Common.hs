d0d851cf605774c867b00ee1b5de1aaf9323a7be
Micah Hahn <micah.s.hahn@gmail.com>
2024-01-29 13:40:30 -0600

    Revert "Fix missing instance in tests"
    This reverts commit 9ce4ae0ddeab3d6183dc60fbb250c6af0540df29.

--- test/Common.hs
+++ test/Common.hs
@@ -1,13 +1,12 @@
 {-# LANGUAGE DataKinds     #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE FlexibleInstances #-}
 module Common where
 
 import           Data.Aeson   (ToJSON)
 import           Data.Proxy   (Proxy (Proxy))
 import           Data.Text    (Text)
-import           Elm          (ElmType(..), ElmDatatype(..), ElmPrimitive(..))
+import           Elm          (ElmType)
 import           GHC.Generics (Generic)
 import           Servant.API  ((:<|>), (:>), Capture, Get, GetNoContent, Header,
                                Headers, JSON, NoContent, Post, PostNoContent,
@@ -29,9 +28,6 @@ newtype Id = Id Int
   deriving (Generic)
 
 instance ElmType Id
-
-instance ElmType (Headers a String) where
-  toElmType _ = ElmPrimitive EString
 
 type TestApi =
        "one"
